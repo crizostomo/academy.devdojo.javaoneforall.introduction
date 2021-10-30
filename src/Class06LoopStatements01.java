079209458d939c88f149ee5c0e2e5120a3e172dd
crizostomo <diogo_crizostomo@hotmail.com>
2021-10-30 18:03:00 -0300

    DevDojo Projects

---
+++ src/Class06LoopStatements01.java
@@ -0,0 +1,8 @@
+public class Class06LoopStatements01 {
+    public static void main(String[] args) {
+        int counter = 10;
+        do {
+            System.out.println("The value of the value is " + ++counter);
+        } while (counter < 20);
+    }
+}
---
+++ src/Class08MultidimensionalArrays01.java
@@ -0,0 +1,19 @@
+public class Class08MultidimensionalArrays01 {
+    public static void main(String[] args) {
+        int [][] days = new int[3][3];
+        days [0][0] = 31;
+        days [0][1] = 28;
+        days [0][2] = 31;
+        days [1][0] = 30;
+        days [1][1] = 31;
+        days [1][2] = 30;
+        days [2][0] = 31;
+        days [2][1] = 30;
+        days [2][2] = 31;
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                System.out.println(days[i][j]);
+            }
+        }
+    }
+}
