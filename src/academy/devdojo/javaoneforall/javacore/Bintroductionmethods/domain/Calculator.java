60d8d7da4d406a363cbac9413fbe348d1e373401
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-01 18:55:39 -0300

    new codes have been added

--- src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
+++ src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
@@ -20,4 +20,28 @@ public class Calculator {
         }
         return num1 / num2;
     }
+
+    public double divideTwoNumbers02(double num1, double num2){
+        if(num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void printTwoNumbersDivided(double num1, double num2){
+        if(num2 == 0){
+            System.out.println("Can't divide by 0");
+            return;
+        }
+            System.out.println(num1 / num2);
+
+    }
+
+    public void changeTwoNumbers(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Inside changeTwoNumbers");
+        System.out.println("Num1 " + num1);
+        System.out.println("Num2 " + num2);
+    }
 }
