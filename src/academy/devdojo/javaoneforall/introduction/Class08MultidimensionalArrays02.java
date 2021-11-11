3ccdde403096dd3123af4e6e45fc86e6be499ade
crizostomo <diogo_crizostomo@hotmail.com>
2021-11-11 10:51:19 -0300

    New codes added and updated

--- src/academy/devdojo/javaoneforall/introduction/Class08MultidimensionalArrays02.java
+++ src/academy/devdojo/javaoneforall/introduction/Class08MultidimensionalArrays02.java
@@ -3,6 +3,7 @@ package academy.devdojo.javaoneforall.introduction;
 public class Class08MultidimensionalArrays02 {
     public static void main(String[] args) {
         int [][] array = new int[3][];
+
         int [] numbers = {1, 2, 3};
 
         array[0] = new int[]{1, 2};
