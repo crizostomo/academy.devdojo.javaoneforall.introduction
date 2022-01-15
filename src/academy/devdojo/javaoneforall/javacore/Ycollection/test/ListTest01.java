3c749fc273fe9992e0c14ad1948c907902536600
crizostomo <diogo_crizostomo@hotmail.com>
2022-01-15 07:32:28 -0300

    exercises have been updated

--- src/academy/devdojo/javaoneforall/javacore/Ycollection/test/ListTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Ycollection/test/ListTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.javaoneforall.javacore.Ycollection.test;
 
 //166 - Collection pt 06 - List pt 01
+//167 - Collection pt 07 - List pt 02
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,17 +9,26 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> names = new ArrayList<>();
+        List<String> names2 = new ArrayList<>();
         names.add("Diogo");
         names.add("Almeida");
+        names2.add("Dev");
+        names2.add("Jr.");
+//        System.out.println(names.remove(0));
 
+        names.addAll(0, names2);
         for(String name : names){
             System.out.println(name);
         }
         names.add("Mr.");
         System.out.println("-------------");
 
-        for(int i = 0; i < names.size(); i++){
+        int size = names.size();
+        for(int i = 0; i < size; i++){
             System.out.println(names.get(i));
+            names.add("Crizostomo");
         }
+        System.out.println(names);
+
     }
 }
